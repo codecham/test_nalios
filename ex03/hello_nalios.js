007f4f9af5c028020f9a7a948f7df64ae27d0e8d
Corentin Dechamps <corentin@Corentins-MacBook-Pro.local>
2025-08-12 16:22:05 +0200

    correction ex03 too_laaaaate

--- ex03/hello_nalios.js
+++ ex03/hello_nalios.js
@@ -1,5 +1,15 @@
+/*
+Etant donné que je n'avais pas compris qu'il fallait utilisé le ascii et que j'ai vu la réponse un poil trop tard,
+j'ai corrigé la réponse quelques minutes après 16h. 
+
+De base comme je n'avais pas compris la question j'avais fais un simple console.log("Hello Nalios !") mais cela me parraissait suspect que ce soit aussi simple.
+
+Voici donc la version avec la valeur en ascii des caractère mais je voulais être honnête sur le fait que la correction de cet exercice à été fait un poil après 16h et vous laisser juger ce qu'il en convient :)
+*/
+
 function hello() {
-	console.log("Hello, Nalios !");
+	asciiMessage = [72, 101, 108, 108, 111, 44, 32, 78, 97, 108, 105, 111, 115, 32, 33];
+	console.log(String.fromCharCode(...asciiMessage));
 }
 
 hello()
